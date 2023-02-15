17e1f8d56eb62ad6c2bfd60a82376bd964bb0bad
Manoj Desai <desaimanoj052@gmail.com>
2023-02-15 17:26:21 +0530

    multiple routes and controllers created

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express=require('express');
 const router= express.Router();
 const homeController = require('../controllers/home_controllers');
 
-router.get('/', homeController.home);
-
 console.log('router loaded');
+
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
 
 module.exports= router;
