2e45432a4e7884c900b468e02f876eca8e5f85e0
Manoj Desai <desaimanoj052@gmail.com>
2023-02-17 08:50:48 +0530

    create sign-up

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const cookieParser = require('cookie-parser');
 const express=require('express');
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
-
+const db = require('./config/mongoose');
 app.use(express.static('./assets'));
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
 
 app.use(expressLayouts);
 
